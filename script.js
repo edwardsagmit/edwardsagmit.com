21a18ca79a9b4f058b6c183079d84f1111f8bc94
clyne <edward.sagmit@gmail.com>
2026-05-22 22:37:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,28 +21,4 @@ themeToggle.addEventListener("click", () => {
     localStorage.setItem("theme", "light");
     themeToggle.setAttribute("aria-label", "Switch to dark mode");
   }
-});const themeToggle = document.getElementById("theme-toggle");
-const toggleIcon = document.querySelector(".toggle-icon");
-
-const savedTheme = localStorage.getItem("theme");
-
-if (savedTheme === "dark") {
-  document.body.classList.add("dark-mode");
-  toggleIcon.textContent = "☾";
-} else {
-  toggleIcon.textContent = "☀";
-}
-
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-
-  const isDarkMode = document.body.classList.contains("dark-mode");
-
-  if (isDarkMode) {
-    localStorage.setItem("theme", "dark");
-    toggleIcon.textContent = "☾";
-  } else {
-    localStorage.setItem("theme", "light");
-    toggleIcon.textContent = "☀";
-  }
 });
